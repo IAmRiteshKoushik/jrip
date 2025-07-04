5f4ba0ba0ee55de87124a9153a4e93d9e97e77d4
Ritesh Koushik <riteshkoushik39@gmail.com>
2025-07-04 06:01:36 +0530

    add: Simple rustfmt rules

--- src/main.rs
+++ src/main.rs
@@ -112,10 +112,15 @@ fn get_files(path: &PathBuf) -> Vec<(String, bool)> {
     dirs
 }
 
-fn dir_button_style() -> impl Fn(&iced::Theme, button::Status) -> button::Style {
+fn dir_button_style() -> impl Fn(&iced::Theme, button::Status) -> button::Style
+{
     |_t, _e| button::Style {
         background: None,
-        text_color: iced::Color::from_rgb(3.0 / 255.0, 161.0 / 255.0, 252.0 / 255.0),
+        text_color: iced::Color::from_rgb(
+            3.0 / 255.0,
+            161.0 / 255.0,
+            252.0 / 255.0,
+        ),
         border: Border::default(),
         shadow: Shadow::default(),
     }
